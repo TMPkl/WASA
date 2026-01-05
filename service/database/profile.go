723050f0f66065b6dbab564251e8818db5183b59
Karol Leszynski <karol.leszynski1@gmail.com>
2026-01-05 22:55:32 +0100

    add photo to database db app impl

--- service/database/profile.go
+++ service/database/profile.go
@@ -1,6 +1,9 @@
 package database
 
-import "errors"
+import (
+	"database/sql"
+	"errors"
+)
 
 func (db *appdbimpl) UserExists(username string) (bool, error) {
 	var exists bool
@@ -30,4 +33,42 @@ func (db *appdbimpl) UpdateUsername(oldUsername, newUsername string) error {
 	}
 	_, err = db.c.Exec("UPDATE users SET username=? WHERE username=?", newUsername, oldUsername)
 	return err
+}
+
+func (db *appdbimpl) AddProfilePhoto(username string, photoData []byte) error {
+	//1. uzytkownik istnieje w bazie danych
+	//2. wstaw zdjecie do zdjec
+	//3. wez id zdjecia
+	//4. podepnij ip do profilu
+	exist, err := db.UserExists(username)
+	if err != nil {
+		return errors.New("Database error")
+	}
+	if !exist {
+		return errors.New("User does not exist")
+	}
+	res, err := db.c.Exec("INSERT INTO Users_photos(photo_data) VALUES(?)", photoData)
+
+	if err != nil {
+		return errors.New("Database error")
+	}
+	new_photo_id, _ := res.LastInsertId()
+
+	//sprawdz czy uzytkownik ma zdjecie
+	//A jesli ma to wywal tamto z bazy, dopisz mu ID nowego
+	//B jeśli nie to wpisz mu ID nowego
+	var old_photo_id sql.NullInt64
+	err = db.c.QueryRow("SELECT photo_id FROM users WHERE username=?)", username).Scan(&old_photo_id)
+	if err != nil {
+		return errors.New("Database error")
+	}
+	if old_photo_id.Valid {
+		// update starego zdjecia
+		_, err = db.c.Exec("DELETE FROM Users_photos WHERE photo_id=?", old_photo_id.Int64)
+		///error do przemyslenia co z nim zrobic
+	}
+
+	_, err = db.c.Exec("Update Users SET photo_id=? where username=?", new_photo_id, username) //zapisz nowe id_zdjecia dla usera
+
+	return nil
 }
